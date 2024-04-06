77158f7964efdb6547f7e9ee087162a80e315801
Xander <xeendoomelby@gmail.com>
2024-04-06 14:30:16 -0400

    changed processing's return from dict to string, added results.html

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ def home():
     return render_template('/index.html')
 
 @app.route('/templates/loading', methods=['GET', 'POST'])
-def out():
+def out(potential=None):
     message = dict(request.form)['note']
     print(message)
     potential_diagnoses = scripts.test.out(message)
-    for diag in potential_diagnoses:
-        print(diag)
-    return "Received"
+    return render_template('results.html', potential=potential_diagnoses)
