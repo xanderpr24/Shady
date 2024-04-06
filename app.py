d9f5fc96f9d18cedb6cc8ca56dc3f99a01a301ff
Xander <xeendoomelby@gmail.com>
2024-04-06 12:53:33 -0400

    added basic form dispatch and reception, added index.html

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import scripts.test
 app = Flask(__name__)
 
 @app.route('/')
-def home(name = "Goober"):
-    return render_template('/home.html', name=name)
-    
+def home():
+    return render_template('/index.html')
+
+@app.route('/templates/loading', methods=['GET', 'POST'])
+def out():
+    message = dict(request.form)['note']
+    print(message)
+    #TODO: processing script
+    return "Received"
